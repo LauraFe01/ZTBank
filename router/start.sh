707d49ec670985ee55d37ab8e82ef083d0a2334c
Giansimone Coccia <S1097845@studenti.univpm.it>
2025-05-14 10:59:30 +0200

    Aggiunte regole per snort e squid in start.sh (ancora da testare)

--- router/start.sh
+++ router/start.sh
@@ -8,3 +8,21 @@ iptables -A FORWARD -j LOG --log-prefix "ROUTER-LOG: " --log-level 4
 
 # Mostra in tempo reale i log di sistema
 tail -F /var/log/syslog
+
+#!/bin/bash
+
+# Avvio di rsyslog
+#service rsyslog start
+
+# Aggiunta di regole iptables per logging e proxy
+#iptables -A FORWARD -j LOG --log-prefix "ROUTER-LOG: " --log-level 4
+#iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3128
+
+# Avvio di Snort
+#snort -c /etc/snort/snort.conf -i eth0 &
+
+# Avvio di Squid
+#service squid start
+
+# Mostra in tempo reale i log di sistema
+#tail -f /var/log/syslog /var/log/snort/alert /var/log/squid/access.log
