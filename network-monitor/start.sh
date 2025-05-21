cfb7e9391df4e1c5d19028813ce170fe352d311f
MicolZazzarini <micol.zazzarini3@gmail.com>
2025-05-21 18:29:28 +0200

    Divisi i servizi di monitoraggio reti e router in due containers separati

--- network-monitor/start.sh
+++ network-monitor/start.sh
@@ -52,30 +52,13 @@ snort -i eth1 -A fast -c /etc/snort/snort.conf -l /var/log/snort/eth1 > /var/log
 echo "Avvio Snort su eth0..."
 snort -i eth0 -A fast -c /etc/snort/snort.conf -l /var/log/snort/eth0 > /var/log/snort/snort_eth0.log 2>&1 &
 
-# Avvia Flask (modifica il percorso secondo dove si trova app.py)
-echo "Avvio Flask server..."
-python3 /router/api/app.py &
-
 # Avvia Squid in foreground
 echo "Avvio Squid..."
 squid -NYCd 1 &
 SQUID_PID=$!
 
 # Abilita IP forwarding
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-# Reset iptables
-iptables -t nat -F POSTROUTING
-iptables -t nat -F PREROUTING
-
-# Default: nega l’accesso al DB (porta 5432), in uanto dobbiamo controllare dinamicamente chi può accedere
-iptables -A FORWARD -p tcp --dport 5432 -j DROP
-
-# NAT e redirect HTTP verso Squid
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j REDIRECT --to-port 3128
-iptables -t nat -A PREROUTING -i eth2 -p tcp --dport 80 -j REDIRECT --to-port 3128
-iptables -t nat -A PREROUTING -i eth3 -p tcp --dport 80 -j REDIRECT --to-port 3128
+# echo 1 > /proc/sys/net/ipv4/ip_forward
 
 echo "Setup completato con successo!"
 
